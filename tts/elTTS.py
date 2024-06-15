79f86be2a55298f4bf0857704f4abc62d8c72b5f
Michal Kwiatkowski <kwiatkowski.michal.2003@gmail.com>
2024-06-15 19:09:58 +0200

    tts init

---
+++ tts/elTTS.py
@@ -0,0 +1,38 @@
+from elevenlabs import save
+from elevenlabs.client import ElevenLabs
+import configparser
+
+
+class TTS(object):
+
+
+    def __init__(self):
+        self.cnf = configparser.ConfigParser()
+        self.cnf.read('config.ini')
+
+    def generate(self, text_filepath):
+        # read file
+        with open(text_filepath, 'r') as file:
+            flashcard = file.read()
+
+        # opening client
+        client = ElevenLabs(
+            api_key=self.cnf['TTS_CONFIG']['ELEVENLABS_API_KEY']  # Defaults to ELEVEN_API_KEY
+        )
+
+        # generate audio
+        audio = client.generate(
+            text=flashcard,
+            voice="Antoni",
+            model="eleven_multilingual_v2"
+        )
+
+        # Save audio to MP3
+        output_filename = 'output_audio.mp3'
+        save(audio, output_filename)
+
+
+if __name__ == '__main__':
+    tts = TTS()
+    tts.generate("example_flashcard.txt")
+
